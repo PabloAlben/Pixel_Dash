0bdb3b6fbee249bb8d27fa46018b676bbf0045b7
Pabloalben <paalca02@xuqueralumnat.es>
2025-03-05 12:06:17 +0100

    Salto del personaje variable añadido y modificaciones en las hitbox

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class MovimientoJugador : MonoBehaviour
     public float velocidad = 5f;
     public float fuerzaSalto = 8f;
     public LayerMask suelo;
+    
     private Rigidbody2D rb;
     private bool enSuelo;
 
@@ -20,14 +21,37 @@ public class MovimientoJugador : MonoBehaviour
 
     void Update()
     {
-        float movimiento = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(movimiento * velocidad, rb.velocity.y);
+       
 
         enSuelo = Physics2D.OverlapCircle(detectorSuelo.position, radioDeteccion, suelo);
 
         if (Input.GetKeyDown(KeyCode.Space) && enSuelo)
         {
             rb.velocity = new Vector2(rb.velocity.x, fuerzaSalto);
+        }
+
+        
+        if (Input.GetKeyUp(KeyCode.Space) && rb.velocity.y > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }
+
+    void FixedUpdate()
+    {
+         float movimiento = Input.GetAxis("Horizontal");
+
+        
+        float velocidadActual = enSuelo ? velocidad : Mathf.Max(velocidad, velocidad * 1.2f);
+        rb.velocity = new Vector2(movimiento * velocidadActual * Time.deltaTime * 60, rb.velocity.y);
+        
+        if (!enSuelo && rb.velocity.y < 0)
+        {
+            rb.gravityScale = 3f; 
+        }
+        else
+        {
+            rb.gravityScale = 2f; 
         }
     }
 }
